56868bdf7a202e09963ea3895e4ca1447c4442fb
adwaith027 <adwaithh227@gmail.com>
2026-01-17 07:55:02 +0530

    added signals to app config

--- Backend/TicketAppB/apps.py
+++ Backend/TicketAppB/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TicketappbConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'TicketAppB'
+
+    # Import signals when Django starts
+    def ready(self):
+        import TicketAppB.signals
